37d6494d265f3003fbc973e459b51fc9e9c2f8fb
Huque, Zeba Fariha <zhuque3@gatech.edu>
2022-04-09 22:52:20 -0400

    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ with open('Crops-2017.csv', 'r') as csvfile:
             if (col[0] == county and col[1] == state):
                 first = col[colCounter]
             if (col[1] == state):
-                totalCrop1 += col[colCounter]
+                totalCrop1 = totalCrop1 + col[colCounter]
         
 
 with open('Crops-2012.csv', 'r') as csvfile:
